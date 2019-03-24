1fc06a69d1511065dac4e05689d06657d463ff8d
BW <klemidz@gmail.com>
2019-03-24 01:31:48 +0100

    aaaezae

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const uuid = require('uuid');
 const logger = require('./middleware/logger');
 
 const app = express();
@@ -15,12 +16,22 @@ const PORT = process.env.PORT || 1337;
 // Init middleware
 app.use(logger);
 
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}));
+
 // Members api route
 app.use('/api/members', require('./routes/api/members'));
 
 
 app.get("/", (req,res)=>{
   res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+
+// Create member
+app.post("/", (req,res)=>{
+  res.send(req.body);
 });
 
 
--- routes/api/members.js
+++ routes/api/members.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const uuid = require('uuid');
 const members = require('../../members');
 
 // Get all members
@@ -15,6 +16,21 @@ router.get('/:id', (req, res)=>{
     res.status(400);
     res.json({error : "Member not found"});
   }
+});
+
+router.post('/', (req, res)=>{
+  const newMember = {
+    id: uuid.v4(),
+    name: req.body.name,
+    country: req.body.country,
+    status: "active"
+  };
+  if(!newMember.name || !newMember.country){
+    res.status(400).json({ error : "Please include a name and country"});
+  }else{
+    members.push(newMember);
+  }
+  res.json(members);
 });
 
 module.exports = router;
